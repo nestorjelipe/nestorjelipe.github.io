21bdc00a98d5167b6b3a72d63734f92a17f718d3
nestorjelipe <nestormartinez@Nestors-MBP.fritz.box>
2016-10-15 19:43:56 +0200

    zurück

--- jsbin.vobofi.js
+++ jsbin.vobofi.js
@@ -58,12 +58,12 @@ L.control.panelLayers(baseLayers, null, {
       }
     }
     
-    var xyz = getTileURL(map.getCenter(), map.getZoom());  
-    var xyzOld = getXYZ(map.getCenter(), map.getZoom() );
+    var xyzNEW = getTileURL(map.getCenter(), map.getZoom());  
+    var xyz = getXYZ(map.getCenter(), map.getZoom() );
     var url = item.layer.getTileUrl( xyz );
 
+    console.log(xyzNEW);
     console.log(xyz);
-    console.log(xyzOld);
       
     var node = L.DomUtil.create('div','panel-thumb');
 
