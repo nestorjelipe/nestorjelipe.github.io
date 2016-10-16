bc518def9870bea6b217bd74616663861fb67d54
nestorjelipe <nestormartinez@Nestors-MBP.fritz.box>
2016-10-16 19:33:35 +0200

    Revert "icon"
    This reverts commit ae92508441b1d6d2be4a773bcbb4c74dea33df53.

--- jsbin.vobofi.js
+++ jsbin.vobofi.js
@@ -24,7 +24,6 @@ var baseLayers = [
 var overlayers = [
   {
     name: "Marker",
-    icon: '<i class="icon icon-water"></i>',
     layer: L.geoJson(marker)
   }
 ];
@@ -115,7 +114,7 @@ map.on('click', function(e) {
     }).addTo(map);*/
 
 var marker = new L.Marker(myCenter);
-    /*map.addLayer(marker);*/
+    map.addLayer(marker);
     marker.bindPopup("<b>I am a Marker </b><br>I could also be a statue");
     
 var polygon = L.polygon([
